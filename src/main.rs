f5e2910c6e1bf1e81b9e92bc5520f14e5313272e
Aleksandar Zivkovic <a.zivkocommits@pm.me>
2024-04-17 14:07:20 +0200

    Add Window and Button
    Working solution

--- src/main.rs
+++ src/main.rs
@@ -1,57 +1,180 @@
+/*#![cfg(windows)]
+#![windows_subsystem = "windows"]*/
+
 use std::mem;
-use std::time::SystemTime;
-use chrono::{DateTime, Local};
-use windows::core::*;
-use windows::Win32::Foundation::SYSTEMTIME;
-use windows::Win32::Globalization::{GetTimeFormatEx, TIME_NOSECONDS, GetDateFormatEx, DATE_AUTOLAYOUT, DATE_SHORTDATE, LOCALE_NAME_SYSTEM_DEFAULT, GetDateFormatW, DATE_LONGDATE, GetLocaleInfoEx, LOCALE_SNAME, TIME_FORMAT_FLAGS};
-use windows::Win32::System::SystemInformation::*;
-use windows::Win32::UI::WindowsAndMessaging::*;
+use windows::{
+    core::*,
+    Win32::{
+        Foundation::SYSTEMTIME,
+        System::SystemInformation::*,
+        UI::{
+            WindowsAndMessaging::*
+        },
+        Globalization::{
+            GetTimeFormatEx,
+            GetDateFormatEx,
+            DATE_LONGDATE,
+            TIME_FORMAT_FLAGS
+        },
+    },
+};
 use registry::{
     Hive,
     Security
 };
-use windows::Win32::System::SystemServices::LOCALE_NAME_MAX_LENGTH;
+use windows::Win32::Foundation::{COLORREF, HINSTANCE, HMODULE, HWND, LPARAM, LRESULT, WPARAM};
+use windows::Win32::Graphics::Gdi::{BeginPaint, COLOR_BACKGROUND, COLOR_WINDOW, CreateSolidBrush, DrawTextW, DT_CENTER, DT_SINGLELINE, DT_VCENTER, DT_WORDBREAK, FillRect, GetSysColor, InvalidateRect, PAINTSTRUCT, RGBQUAD};
+use windows::Win32::System::LibraryLoader::{GetModuleHandleA};
+use once_cell::unsync::Lazy;
+
+static mut DISPLAY_TEXT : String = String::new();
+static mut TEXT_UTF16: Lazy<Vec<u16>> = Lazy::new(|| get_output_string().encode_utf16().collect());
+
+
 
 fn main() {
     unsafe {
-        // Get Date Time Format Settings From Current User
+        let date = get_date_long_format();
+        let time = get_time_format();
+        let date_time_format = get_registry_values_from_current_user();
 
-        // Hive that contains required infos
-        let regkey = Hive::CurrentUser
-            .open("Control Panel\\International", Security::Read)
-            .unwrap();
+        let display_string = format!("Current date: {} Current time: {} \r\n Equals Registry Settings: {}",
+                                     &date,
+                                     &time,
+                                     &date_time_format
+        );
+        println!("{}",display_string);
 
-        //current time
+        create_window();
+
+    }
+
+}
+
+fn get_output_string() -> String {
+    unsafe {
+    return format!("Current date: {} Current time: {} \r\n Equals Registry Settings: {}",
+                   get_date_long_format(),
+                   get_time_format(),
+                   get_registry_values_from_current_user());
+        }
+}
+
+fn create_window() -> Result<HWND> {
+    unsafe {
+        let instance = GetModuleHandleA(None).unwrap();
+        debug_assert!(instance.0 != 0);
+
+        let class_name = PCSTR("window_class".as_ptr());
+
+        let wc = WNDCLASSA {
+            hInstance: HINSTANCE::from(instance),
+            hCursor: LoadCursorW(None, IDC_ARROW)?,
+            lpszClassName: PCSTR(class_name.as_ptr()),
+            style: CS_HREDRAW | CS_VREDRAW,
+            lpfnWndProc: Some(window_proc),
+            ..Default::default()
+        };
+
+        let reg = RegisterClassA(&wc);
+        debug_assert!(reg != 0);
+
+        let window = CreateWindowExA(
+            Default::default(),
+            class_name,
+            PCSTR("Demo OS Date & Time".as_ptr()),
+            WS_OVERLAPPEDWINDOW | WS_VISIBLE,
+            200,
+            200,
+            800,
+            200,
+            None,
+            None,
+            instance,
+            None
+        );
+
+        let mut message = std::mem::zeroed();
+        while GetMessageA(&mut message, None, 0, 0).into() {
+            TranslateMessage(&message);
+            DispatchMessageA(&message);
+        }
+
+        ShowWindow(window, SW_SHOW);
+
+        create_button(window);
+
+        Ok(window)
+    }
+}
+
+unsafe fn create_button(window: HWND) -> Result<HWND> {
+    let refresh_button = CreateWindowExA(
+        Default::default(),
+        PCSTR("BUTTON".as_ptr()),
+        PCSTR("Refresh".as_ptr()),
+        WS_TABSTOP | WS_VISIBLE | WS_CHILD | WINDOW_STYLE(BS_DEFPUSHBUTTON as u32),
+        10,
+        10,
+        10,
+        10,
+        window,
+        None, // Button ID
+        GetModuleHandleA(None).unwrap(),
+        None,
+    );
+        Ok(refresh_button)
+}
+
+
+unsafe fn get_date_long_format() -> String {
+    let mut system_time : SYSTEMTIME = mem::zeroed();
+    let system_time = GetLocalTime();
+
+    let buffer_length = GetDateFormatEx(
+        Option::None,
+        DATE_LONGDATE,
+        Option::from(&system_time as *const SYSTEMTIME),
+        PCWSTR::null(),
+        Option::None,
+        Option::None);
+
+    //let buffer : &mut [u16] = &mut [];
+    let mut buffer: Vec<u16> = vec![0; buffer_length as usize];
+
+
+
+    GetDateFormatEx(
+        Option::None,
+        DATE_LONGDATE,
+        Option::from(&system_time as *const SYSTEMTIME),
+        PCWSTR::null(),
+        Some(&mut buffer[..]),
+        Option::None);
+
+    println!("{}", String::from_utf16_lossy(&buffer));
+
+    return String::from_utf16_lossy(&buffer);
+
+}
+
+fn get_registry_values_from_current_user() -> String {
+    let regkey = Hive::CurrentUser
+        .open("Control Panel\\International", Security::Read)
+        .unwrap();
+
+    return  format!(
+        "Dateformat: {}, Timeformat: {}",
+        regkey.value("sLongDate").unwrap(),
+        regkey.value("sTimeFormat").unwrap(),
+    );
+}
+
+fn get_time_format() -> String {
+    unsafe {
         let mut system_time : SYSTEMTIME = mem::zeroed();
         let system_time = GetLocalTime();
         const LOCALE_NAME_USER_DEFAULT: PCWSTR = PCWSTR::null();
-        //pub const LOCALE_NAME_USER_DEFAULT: PCWSTR = w!("!x-sys-default-locale");
-
-        //GetLocaleInfoEx(LOCALE_NAME_USER_DEFAULT, LOCALE_SNAME, )
-
-        let buffer_length = GetDateFormatEx(
-            Option::None,
-            DATE_LONGDATE,
-            Option::from(&system_time as *const SYSTEMTIME),
-            PCWSTR::null(),
-            Option::None,
-            Option::None);
-
-        //let buffer : &mut [u16] = &mut [];
-        let mut buffer: Vec<u16> = vec![0; buffer_length as usize];
-
-        let locale = format!("{}", regkey.value("LocaleName").unwrap());
-        let locale2 = "en-US\0".encode_utf16();
-
-
-
-        GetDateFormatEx(
-            Option::None,
-            DATE_LONGDATE,
-            Option::from(&system_time as *const SYSTEMTIME),
-            PCWSTR::null(),
-            Some(&mut buffer[..]),
-            Option::None);
 
 
         let time_buffer_size = GetTimeFormatEx(
@@ -71,38 +194,63 @@ fn main() {
             Some(&mut time_buffer[..]));
 
 
-        println!("{}", String::from_utf16_lossy(&buffer));
+
         println!("{}", String::from_utf16_lossy(&time_buffer));
 
+        return String::from_utf16_lossy(&time_buffer);
+    }
+}
 
 
+unsafe extern "system" fn window_proc(
+    window: HWND,
+    message: u32,
+    wparam: WPARAM,
+    lparam: LPARAM,
+) -> LRESULT {
 
+    match message {
+        WM_PAINT => {
+            let mut ps = PAINTSTRUCT::default();
+            let hdc = BeginPaint(window, &mut ps);
+            let rect = &mut ps.rcPaint;
 
+            let background_brush = CreateSolidBrush(COLORREF(GetSysColor(COLOR_WINDOW)));
+            FillRect(hdc, rect, background_brush);
 
-        //date time formats - only shortform
+            DrawTextW(hdc, &mut *TEXT_UTF16, rect,  DT_CENTER | DT_VCENTER);
+            LRESULT(0)
+        }
+        WM_CREATE => {
+            CreateWindowExA(
+                Default::default(),
+                PCSTR(b"BUTTON\0".as_ptr()),
+                PCSTR(b"Refresh\0".as_ptr()),
+                WS_VISIBLE | WS_CHILD | WINDOW_STYLE(BS_PUSHBUTTON as u32),
+                600,
+                125,
+                50,
+                25,
+                window,
+                HMENU(21), // Cast ID to HMENU
+                GetModuleHandleA(None).unwrap(),
+                None);
+            LRESULT(0)
+        }
+        WM_COMMAND => {
+            if (wparam.0 as u32) == 21 {
+                println!("{}", "Refreshing ...");
+                TEXT_UTF16 = mem::zeroed();
+                TEXT_UTF16= Lazy::new(|| get_output_string().encode_utf16().collect());
+                InvalidateRect(window, None, true);
+            }
+            LRESULT(0)
+        }
 
-        let date_time_format = format!(
-            "Dateformat: {}, Timeformat: {}",
-            regkey.value("sShortDate").unwrap(),
-            regkey.value("sTimeFormat").unwrap(),
-        );
-
-        let dt_local: DateTime<Local> = SystemTime::now().into();
-
-        println!("{}", dt_local);
-
-        //println!("{}", String::from(&date));
-
-        // TODO use long form as well ?
-
-        println!("{}", date_time_format);
-
-
-        MessageBoxA(
-            None,
-            PCSTR(date_time_format.as_ptr()),
-            s!("Date Time Demo"),
-            MB_OK,
-        );
+        WM_DESTROY => unsafe {
+            PostQuitMessage(0);
+            LRESULT(0)
+        }
+        _ => DefWindowProcA(window, message, wparam, lparam),
     }
 }
